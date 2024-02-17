444bae50a7e70f72f9239269940cb2452e78389f
Aibar Tolegeno <aibartolegeno.2283@gmail.com>
2024-02-17 21:26:40 +0600

    Beginning

---
+++ src/sections/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
+
+function Home() {
+  return (
+    <div>
+        <Navbar></Navbar>
+        <h1>Hello I'm Tolegenov Aibar</h1>
+        <p>I'm a ambitious front end developer </p>
+    </div>
+  )
+}
+
+export default Home
