e4c6ecf68b098891eb049c26a70dac15f06ecaf7
Aibar Tolegeno <aibartolegeno.2283@gmail.com>
2024-06-07 16:11:11 +0500

    restyling

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -1,30 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Experience() {
+  const [exp, setExp] = useState([]);
+
+  useEffect(() => {
+    setExp([
+      {
+        title: "Web Dev Mentor",
+        details: "IT club | 2022 - 2023",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, blanditiis.",
+      },
+      {
+        title: "Infomatrix Asia",
+        details: "Almaty | June 2022",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, blanditiis.",
+      },
+      {
+        title: "Mentor in Talimger",
+        details: "Volunteer | 2022 - 2023",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, blanditiis.",
+      },
+      {
+        title: "Hackathons",
+        details: "Oskemen, Semey | 2022 - 2023",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, blanditiis.",
+      },
+    ]);
+  }, []);
   return (
     <section>
       <div className="container p-10 flex flex-col gap-4 items-center justify-center">
         <h2 className="title">Experience</h2>
         <div className="flex items-center justify-between gap-24">
           <div className="flex flex-col gap-4">
-            <div>
-              <h2 className="text-xl font-bold">Web Dev Mentor</h2>
-              <p>IT club | 2022 - 2023</p>
-            </div>
-            <div>
-              <h2 className="text-xl font-bold">Infomatrix Asia</h2>
-              <p>Almaty | June 2022</p>
-            </div>
-            <div>
-              <h2 className="text-xl font-bold">Mentor in Talimger</h2>
-              <p>Volunteer | 2022 - 2023</p>
-            </div>
-            <div>
-              <h2 className="text-xl font-bold">Hackathons</h2>
-              <p>Oskemen, Semey | 2022 - 2023</p>
-            </div>
+            {exp.map((item) => (
+              <div>
+                <h2 className="text-xl font-bold">{item.title}</h2>
+                <p>{item.details}</p>
+              </div>
+            ))}
           </div>
-          <div className="border-8 border-white p-32 rounded-md">Display</div>
+
+          <div className="border-4 border-slate-900 p-2 rounded-md w-64 h-64">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
+              blanditiis.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
+              blanditiis.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
+              blanditiis.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
+              blanditiis.
+            </p>
+          </div>
         </div>
       </div>
     </section>
